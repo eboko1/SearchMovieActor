9ff9591f0bd381cfaba7722f7adf39e832d2a19c
Vika <eboko1@gmail.com>
2017-10-30 23:38:17 +0200

    add MovieAdapter end init MainActivity

--- app/src/main/java/com/example/vika/searchmovieactor/utils/Constants.java
+++ app/src/main/java/com/example/vika/searchmovieactor/utils/Constants.java
@@ -7,7 +7,7 @@ package com.example.vika.searchmovieactor.utils;
 public class Constants {
     public static final class HTTP {
         public static final String BASE_URL = "https://api.themoviedb.org/";
-        public static final String API_KEY = "70e74e0ce9cf659b776cb0aed6073a7e";
+        public static final String API_KEY = "";
         public static final String IMAGE_URL_BASE_PATH = "https://image.tmdb.org/t/p/w300/";
     }
 }
